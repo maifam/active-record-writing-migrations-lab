d944aff9398aa085946d19d27c7680c7b100cfd0
Mai Pham <maitphm@gmail.com>
2020-11-29 19:07:46 -0500

    Done.

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,7 @@
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+
+    def change(table_name, column_name, type)
+        change_column :students, :birthdate, :datetime
+    end 
+
+end 
